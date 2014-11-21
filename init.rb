a73083006151adc2afbd9b464f4cfb1f478d7090
Jan Hovancik <conta.srdr@gmail.com>
2014-11-21 18:38:40 +0100

    init

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :project_author_plugin do
+  name 'Project Author Plugin plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
